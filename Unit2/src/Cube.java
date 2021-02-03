5d2829cae09a38875af907bfb0d413fdbdd0c14a
Cat1029 <44483272+Cat1029@users.noreply.github.com>
2021-02-02 17:59:38 -0800

    Unit2

---
+++ Unit2/src/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Catherine Du
+//Date - 2/2/2021
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return 6*side*side;
+	}
+}
---
+++ Unit2/src/Trap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Catherine Du
+//Date - 2/2/2021
+//Class - APCSA
+//Lab  - Trapezoid Area
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+
+		return .5 * (base1 + base2) * height;
+
+	}
+}
